fc24f1cdca12291d4db5bedb538a0b510c387e06
dewi-xaltius <smdewi@xaltius.tech>
2025-07-13 13:40:35 +0800

    Createad Custom Hooks useAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import './App.css'
+import './App.css';
+import useAPI from './useAPI';
 
 function App() {
+  const URL = "https://jsonplaceholder.typicode.com/posts";
+  const { data, loading, error } = useAPI(URL);
   
-
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error loading posts!</p>;
+  
   return (
-    <>
-      <div className="App">
-        <h1>Custom Hooks</h1>
+    <div className="App">
+      <h1>Posts</h1>
+      <div>
+        {data.map(post => (
+          <button key={post.id}>{post.title}</button>
+        ))}
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
